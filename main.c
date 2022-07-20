2443428e34ca0832e476db90f4061a2354843932
Olivier Clamens <olivier.clamens@u-psud.fr>
2022-07-20 19:26:36 +0200

    int -> bin in string

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+
 int main()
 {
-    puts("Hello World");
+    int nb=164;
+    unsigned char nbBin[33];
+    int i,format=8;
+
+    for(i=0;i<format;i++){
+        nbBin[i]=((nb>>(format-1-i))&0x01)+0x30;
+    }
+    nbBin[i]=0;
+
+    printf("%s",nbBin);
+    
     return 0;
 }
